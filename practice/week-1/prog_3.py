9cacb92f69db33a4cf5df0c6578222c35120398f
Muhammad Saad Maqbool <saad20106858@gmail.com>
2026-06-29 12:33:32 +0500

    Delete prog_3.py

--- practice/week-1/prog_3.py
+++
@@ -1,4 +0,0 @@
-# This Program can be used to print whether the 1st number is greater than 2nd or not
-a=input("enter 1st number: ")
-b=input("enter 2nd number: ")
-print("1st number is greater than 2nd number is ",a>b)
