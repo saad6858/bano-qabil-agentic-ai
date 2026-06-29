48d66b01f63464d8ea683034e5de5c9cf4832432
Muhammad Saad Maqbool <saad20106858@gmail.com>
2026-06-29 12:33:42 +0500

    Delete prog_4.py

--- practice/week-1/prog_4.py
+++
@@ -1,4 +0,0 @@
-# This program can be used to find the remainder when 2 numbers are divided
-a=int(input("enter the number you want to divide: "))
-b=int(input("enter the number you want to divide by: "))
-print("The remainder after division is: ",a%b)
