f843a4258a5d51a848ef94251350d0744f4d4311
Muhammad Saad Maqbool <saad20106858@gmail.com>
2026-07-06 11:59:46 -0700

    Delete display.py

--- assignments/display.py
+++
@@ -1,7 +0,0 @@
-import grade
-def display_result(name,marks):
-	grade_ = grade.calculate_grade(marks)
-	print("---- Student Result ----")
-	print("Name:",name)
-	print("Marks:",marks)
-	print("grade:",grade_)
