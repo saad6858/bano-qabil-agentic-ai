73b096bf9bbd5474395a430dd501f08e939f967e
Muhammad Saad Maqbool <saad20106858@gmail.com>
2026-07-06 12:00:07 -0700

    Delete main.py

--- assignments/main.py
+++
@@ -1,8 +0,0 @@
-inport display
-while True:
-	name=input("Enter student name: ")
-	marks=int(input("Enter your marks in percentage: "))
-	display.display_result(name,marks)
-	choice=input("Is there any other student? (yes/no): ")
-	if(choice.lower() != "yes"):
-		break
